fbedb5d2d26b5b7703cfc7749aec7a77c2abc393
Idace <idaebeling@gmail.com>
2025-05-13 11:17:43 +0200

    Text/name

--- my-react-app/src/components/DogImg.jsx
+++ my-react-app/src/components/DogImg.jsx
@@ -13,12 +13,16 @@ const fetchDogImage = async () => {
 };
 
 return (
+    <>
+    <h1 className="head-text">Woof Wall</h1>
+    
     <div className="dog-viewer">
         <h2>Unleash a Random Dog!</h2>
         <button onClick={fetchDogImage}>Show Me the Dog!</button>
 
         {imageUrl && <img src={imageUrl} alt="Random dog" className="dog-img"/>}
     </div>
+    </>
     );
 }
 
